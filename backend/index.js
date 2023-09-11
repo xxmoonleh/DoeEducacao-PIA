1e6eb829fab9b4c7fd02a6918bf7086ca717de09
Hevellyn Gaspar <hevellyn.borges@aluno.ifsp.edu.br>
2023-09-11 09:30:43 -0300

    index.js

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+const { json } = require('express');
 const express = require('express');
 const app = express();
 app.use(express.json())
@@ -16,11 +17,18 @@ app.get('/',(req,res)=>{
 });
 
 const getAllPessoas = async () =>{
-    const [query] = await connection.execute('select * from TestePessoa.Pessoa');
+    const [query] = await connection.execute('select * from testepessoa.pessoa');
     return query;
 }
 
 app.get('/pessoa', async (req,res)=>{
     const consulta = await getAllPessoas();
     return res.status(200).json(consulta);
+})
+
+app.get('/pessoa:id', async (req,res)=>{
+    const {id} = req.params;
+    const [query] = await connection.execute('select * from testepessoa.pessoa where id = ?', [id]);
+    if(query.lenght === 0) return res.status(400).json({mensagem: 'Não Encontrado.'})
+    return res.status(200).json(query);
 })
